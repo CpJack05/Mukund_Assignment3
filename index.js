72b487686a61123eab4e2c81b9612b147ca5b7fd
Mukund Jogani <Mukundjogani@yahoo.com>
2023-07-26 20:15:27 -0400

    Adding server-side validation code to index.js file

--- index.js
+++ index.js
@@ -23,8 +23,89 @@ myApp.get('/', function (req, res) {
     res.render('OnlineStore'); // no need to add .ejs to the file name
 });
 
+var phoneNumberRegex = /^\d{10}$/;
+var positiveNumber = /^[0-9][0-9]*$/;
+var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//function to check that value is valid or not using regular expression
+function checkRegex(userInput, regex) {
+    if (regex.test(userInput)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Phone number validation function
+function customPhoneNumberValidation(value) {
+    if (!checkRegex(value, phoneNumberRegex)) {
+        throw new Error('Phone No. should be in the format : 0123456789');
+    }
+    return true;
+}
+
+//Email validation function
+function customEmialValidation(value) {
+    if (!checkRegex(value, emailRegex)) {
+        throw new Error('Credit Card No. should be in the format : test@test.com');
+    }
+    return true;
+}
+
+//For positive numbers
+function breadValidation(value, {req}){
+    var bread  = req.body.bread;
+
+    if(!checkRegex(value, positiveNumber)){
+        throw new Error('Please enter positive number in a Bread field');
+    }
+    return true;
+}
+
+function milkValidation(value, {req}){
+    var milk = req.body.milk;
+
+    if(!checkRegex(value, positiveNumber)){
+        throw new Error('Please enter positive number in a Milk field ');
+    }
+    return true;
+}
+
+function softdrinkValidation(value, {req}){
+    var softdrink = req.body.softdrink;
+
+    if(!checkRegex(value, positiveNumber)){
+        throw new Error('Please enter positive number in a Soft-Drink field ');
+    }
+    return true;
+}
+
+function checkTotalValue(value, {req})
+{
+    var softdrink = req.body.softdrink;
+    var milk = req.body.milk;
+    var bread  = req.body.bread;
+
+    var subTotal = (softdrink * 3.5) + (milk * 7) + (bread * 2.5);
+
+    if(subTotal < 10)
+    {
+        throw new Error('Sub total must be greater than 10.');
+    }
+    return true;
+}
 
 myApp.post('/', [
+    check('name', 'You must enter your Name').not().isEmpty(),
+    check('email', 'You must enter your e-mail ID').isEmail(),
+    check('phonenumber').custom(customPhoneNumberValidation),
+    check('address', 'You must enter you Address').not().isEmpty(),
+    check('city', 'You must enter your City').not().isEmpty(),
+    check('province', 'You must select Province').not().isEmpty(),
+    check('bread').custom(breadValidation),
+    check('milk').custom(milkValidation),
+    check('softdrink').custom(softdrinkValidation),
+    check('subTotal').custom(checkTotalValue)
 ], function (req, res) {
 
     const errors = validationResult(req);
@@ -83,3 +164,5 @@ myApp.listen(8080);
 
 //tell everything was ok
 console.log('Everything executed fine.. website at port 8080....');
+
+
