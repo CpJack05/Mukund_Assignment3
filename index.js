eaec5da4c56770aea3d1c6d816ba6b3c4d63f2dc
Mukund Jogani <Mukundjogani@yahoo.com>
2023-07-26 20:13:24 -0400

    Getting data from the form and created a object

--- index.js
+++ index.js
@@ -22,3 +22,64 @@ myApp.set('view engine', 'ejs');
 myApp.get('/', function (req, res) {
     res.render('OnlineStore'); // no need to add .ejs to the file name
 });
+
+
+myApp.post('/', [
+], function (req, res) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        //console.log(errors); // check what is the structure of errors
+        res.render('OnlineStore', {
+            errors: errors.array()
+        });
+    }
+    else {
+
+        var name = req.body.name;
+        var email = req.body.email;
+        var address = req.body.address;
+        var phonenumber = req.body.phonenumber;
+        var city = req.body.city;
+        var province = req.body.province;
+        var bread = req.body.bread;
+        var milk = req.body.milk;
+        var softdrink = req.body.softdrink;
+
+        //define local variable
+        var breadCharge = 0;
+        var milkCharge = 0;
+        var softdrinkCharge = 0;
+        var subTotal = 0;
+        var totalAmount = 0;
+        var taxAmount = 0;
+        var tax = 0;
+
+        breadCharge = 2.5 * bread;
+        milkCharge = 7 * milk;
+        softdrinkCharge = 3.5 * softdrink;
+        subTotal = breadCharge + milkCharge + softdrinkCharge;
+
+        var pageData = {
+            name: name,
+            email: email,
+            address: address,
+            phonenumber: phonenumber,
+            city: city,
+            province: province,
+            breadCharge: breadCharge,
+            milkCharge: milkCharge,
+            softdrinkCharge: softdrinkCharge,
+            subTotal: subTotal,
+            taxAmount: taxAmount,
+            totalAmount: totalAmount
+        }
+        res.render('OnlineStore', pageData);
+    }
+});
+
+// start the server and listen at a port
+myApp.listen(8080);
+
+//tell everything was ok
+console.log('Everything executed fine.. website at port 8080....');
